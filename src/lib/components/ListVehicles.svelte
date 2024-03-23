e50e0fe2c0acfa7a81af0fce2663b15de216f148
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 14:02:25 -0400

    Added properties in the ListVehicle

--- src/lib/components/ListVehicles.svelte
+++ src/lib/components/ListVehicles.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
     import { onMount } from 'svelte';
-    import ApiService from '$lib/services/ApiService';
     import type { Vehicle } from "$lib/models/Vehicle";
+	import ApiVehicleService from '$lib/services/ApiVehicleService';
 
     let vehicles: Vehicle[] = [];
-    const api = new ApiService('https://your-api-gateway-url.amazonaws.com/prod');
+    const api = new ApiVehicleService();
 
     onMount(async () => {
         try {
-            vehicles = await api.get('vehicles');
+            vehicles = await api.getVehicles();
         } catch (error) {
             console.error("Failed to fetch vehicles:", error);
         }
@@ -18,7 +18,11 @@
 {#if vehicles.length > 0}
     <ul>
         {#each vehicles as vehicle}
-            <li>{vehicle.name}</li>
+            <li>
+                <span>{vehicle.name}</span>
+                <span>{vehicle.id}</span>
+                <span>Last Used: {vehicle.lastUsed}</span>
+            </li>
         {/each}
     </ul>
 {:else}
