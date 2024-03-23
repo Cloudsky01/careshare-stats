97537d9e66ef7dd06d151ebca5f56994204b0c7a
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 15:48:54 -0400

    Added about placeholder page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,3 @@
 <h1>About this site</h1>
-<a href="/">Home</a>
+<p>This site is dedicated to providing information and statistics about CareShare, a platform for sharing and caring. Our mission is to connect people and facilitate the exchange of resources, knowledge, and support.</p>
+<p>Whether you're looking for assistance or want to contribute to the community, CareShare is here to help. Join us today and make a difference!</p>
