8578d13eb2043cc62834aaf84a2bccb4d279c662
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 15:49:28 -0400

    Added map component in map page

---
+++ src/routes/map/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import ListVehicles from "$lib/components/ListVehicles.svelte";
+    import Map from "$lib/components/Map.svelte";
+</script>
+
+<div class="content">
+    <ListVehicles />
+    <Map />
+</div>
+
+
+
+<style>
+    .content {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      grid-gap: 20px;
+    }
+  
+    @media (max-width: 768px) {
+      .content {
+        grid-template-columns: 1fr;
+        grid-template-rows: auto auto;
+      }
+    }
+  </style>
