bc953e067fe0b12bad274d86fd0e3b3810387467
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 16:44:58 -0400

    Add container for layout and usage of network table

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,5 +2,14 @@
     import NavBar from '$lib/components/NavBar.svelte';
 </script>
 
-<NavBar />
-<slot />
+<div class="app-container">
+    <NavBar />
+    <slot />
+</div>
+
+<style>
+    .app-container {
+      max-height: 100vh; /* Viewport Height */
+      overflow-y: auto; /* Enables scroll only if needed */
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,27 @@
 <script>
-    import UsageOfNetwork from '$lib/components/UsageOfNetwork.svelte';
+	import UsageOfNetworkTable from '$lib/components/UsageOfNetworkTable.svelte';
     import 'bulma/css/bulma.css';
 </script>
 
-<html>
-  <div class="content">
-    <h1>CARSHARE-STATS</h1>
-    <p>Carshare-stats is a simple web application that displays the number of vehicles in a carshare service.</p>
-    <p>It consists of the following pages:</p>
-    <ul>
-      <li><a href="/map">Map</a>: Displays a map with the location of the vehicles.</li>
-      <li><a href="/chart">Chart</a>: Displays a chart with the number of vehicles per day.</li>
-      <li><a href="/about">About</a>: Information about the application.</li>
-      <li><a href="/contact">Contact</a>: Contact information.</li>
-    </ul>
-    <p>Click on the links above to navigate to the different pages.</p>
-    <UsageOfNetwork />
-  </div>
-</html>
+<div class="content">
+  <h1>CARSHARE-STATS</h1>
+  <p>Carshare-stats is a simple web application that displays the number of vehicles in a carshare service.</p>
+  <p>It consists of the following pages:</p>
+  <ul>
+    <li><a href="/map">Map</a>: Displays a map with the location of the vehicles.</li>
+    <li><a href="/chart">Chart</a>: Displays a chart with the number of vehicles per day.</li>
+    <li><a href="/about">About</a>: Information about the application.</li>
+    <li><a href="/contact">Contact</a>: Contact information.</li>
+  </ul>
+  <p>Click on the links above to navigate to the different pages.</p>
+  <h2>Usage of the Network</h2>
+  <p>This table shows the averaged usage of the network per day</p>
+  <UsageOfNetworkTable />
+</div>
 
 <style>
   .content {
-    max-width: 800px;
-    margin: 0 auto;
     padding: 20px;
+    margin: 0 auto;
   }
 </style>
