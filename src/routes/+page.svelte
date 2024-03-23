5be23a7cf9545feb0063984b3a90d139766d1fd5
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 13:09:04 -0400

    Update page.svelte with ListVehicles component and Map styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,27 @@
 <script>
-    import Map from '../lib/components/Map.svelte';
-</script>
-
-<html lang="en">
-    <h1>Welcome to CarShare-stats</h1>
-    <a href="/about">About my site</a>
-    <Map></Map>
-</html>
+    import ListVehicles from '$lib/components/ListVehicles.svelte';
+	import Map from '$lib/components/Map.svelte';
+  </script>
+  
+  <div class="container">
+    <div id="map" style="height: 80vh;">
+        <!-- <Map /> -->
+    </div>
+    <ListVehicles />
+  </div>
+  
+  <style>
+    .container {
+      display: grid;
+      grid-template-rows: 1fr auto;
+      gap: 1rem;
+    }
+  
+    @media (max-width: 768px) {
+      .container {
+        grid-template-rows: auto;
+        grid-template-columns: 1fr;
+      }
+    }
+  </style>
+  
