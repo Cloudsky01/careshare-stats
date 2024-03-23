d1a56a1fedc4a253ec3510ec626741084f05198c
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 16:45:05 -0400

    Add usage of network table to chart page

--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -1,9 +1,16 @@
 <script>
     import NumberOfVehicles from '$lib/components/NumberOfVehiclesChart.svelte';
+	import UsageOfNetworkTable from '$lib/components/UsageOfNetworkTable.svelte';
 </script>
 
 
 <div class="container">
-  <NumberOfVehicles />
+    <div class="content">
+        <h1>Carshare Stats</h1>
+        <p>Carshare Stats is a simple web application that displays the number of vehicles in a carshare service.</p>
+    <NumberOfVehicles />
+    <h2>Usage of the Network</h2>
+</div>
+  <UsageOfNetworkTable />
 </div>
 
