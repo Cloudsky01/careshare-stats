2950d762bb88c588352e838edbf136907137ac56
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 16:14:52 -0400

    Add UsageOfNetwork component

---
+++ src/lib/components/UsageOfNetwork.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { writable } from 'svelte/store';
+    let usage = 50;
+    // Create a writable store for the percentage value
+    let percentageStore = writable(0);
+
+    // Update the store value whenever the usage changes
+    $: {
+        percentageStore.set(Math.min(Math.max(usage, 0), 100));
+    }
+</script>
+
+<p class="usage-number">{$percentageStore}</p>
+
+<style>
+    .usage-number {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100%;
+        font-weight: bold;
+        display: inline-block;
+    }
+</style>
