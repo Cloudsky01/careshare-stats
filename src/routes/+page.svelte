9d7dd3b1cc766f94e54da02a8d44e15b3b780885
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2024-03-23 17:24:50 -0400

    Refactor page layout and add health score display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,15 +5,27 @@
 
 <div class="content">
   <h1>CARSHARE-STATS</h1>
-  <p>Carshare-stats is a simple web application that displays the number of vehicles in a carshare service.</p>
-  <p>It consists of the following pages:</p>
-  <ul>
-    <li><a href="/map">Map</a>: Displays a map with the location of the vehicles.</li>
-    <li><a href="/chart">Chart</a>: Displays a chart with the number of vehicles per day.</li>
-    <li><a href="/about">About</a>: Information about the application.</li>
-    <li><a href="/contact">Contact</a>: Contact information.</li>
-  </ul>
-  <p>Click on the links above to navigate to the different pages.</p>
+  <div class="columns">
+    <div class="column">
+      <p>Carshare-stats is a simple web application that displays the number of vehicles in a carshare service.</p>
+      <p>It consists of the following pages:</p>
+      <ul>
+        <li><a href="/map">Map</a>: Displays a map with the location of the vehicles.</li>
+        <li><a href="/chart">Chart</a>: Displays a chart with the number of vehicles per day.</li>
+        <li><a href="/about">About</a>: Information about the application.</li>
+        <li><a href="/contact">Contact</a>: Contact information.</li>
+      </ul>
+      <p>Click on the links above to navigate to the different pages.</p>
+    </div>
+    <div class="column">
+      <div class="health-container">
+        <h1>GENERAL HEALTH SCORE</h1>
+        <p>Overall health score of the network</p>
+        <p>100%</p>
+      </div>
+    </div>
+  </div>
+  
   <h2>Usage of the Network</h2>
   <p>This table shows the averaged usage of the network per day</p>
   <UsageOfNetworkTable />
@@ -21,7 +33,10 @@
 
 <style>
   .content {
-    padding: 20px;
+    width: 90%;
     margin: 0 auto;
+  }
+  .health-container {
+    border: 5px solid #333;
   }
 </style>
